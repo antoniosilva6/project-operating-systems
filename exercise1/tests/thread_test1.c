8713e73ae7a9d13ba7478575e9b35f2ca8e36f39
Splinter <92955462+antoniosilva6@users.noreply.github.com>
2023-05-04 00:35:05 +0100

    Add files via upload

---
+++ exercise1/tests/thread_test1.c
@@ -0,0 +1,49 @@
+#include "../fs/operations.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#define N_THREADS (5)
+
+int counter = 0;
+
+void test(){
+
+    char *path1 = "/f1";
+    char *path2 = "/f2";
+
+    int f = tfs_open(path1, TFS_O_CREAT);
+    assert(f != -1);
+
+    assert(tfs_close(f) != -1);
+
+    f = tfs_open(path2, TFS_O_CREAT);
+    assert(f != -1);
+
+    assert(tfs_close(f) != -1);
+
+    counter++;
+}
+
+int main(){
+
+    pthread_t tid[N_THREADS];
+
+    assert(tfs_init(NULL) != -1);
+
+    for (int i = 0; i < N_THREADS; ++i)
+        assert(pthread_create(&tid[i], NULL, (void *)test, NULL) == 0);
+
+    for (int i = 0; i < N_THREADS; ++i) 
+        assert(pthread_join(tid[i], NULL) == 0);
+
+    assert(tfs_destroy() != -1);
+
+    assert(counter == N_THREADS);
+
+    printf("Successful test\n");
+    return 0;
+}
